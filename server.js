8f41b3efde4be141d97c6889970704237a9967b8
toqitahmid <rubaiathali@gmail.com>
2026-08-07 11:08:51 +0600

    set express backend for my portfolio

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
