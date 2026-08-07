13c6b62a8a98d3520122b74be7c0c455f9fe0235
toqitahmid <rubaiathali@gmail.com>
2026-08-07 11:38:49 +0600

    add mongodb atlas with express backend

--- server.js
+++ server.js
@@ -1,11 +1,37 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config();
+
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    
+
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+  } catch(error){
+    console.log(error);
+  }
+}
+run().catch(console.dir);
+
+
